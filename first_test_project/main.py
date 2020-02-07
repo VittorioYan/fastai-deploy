d446c824a2bc5911e44d57c93a7e5fb9a4474237
VittorioYan <1209087381@qq.com>
2020-02-07 23:20:18 +0800

    init first_test_porject

---
+++ first_test_project/main.py
@@ -0,0 +1,59 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_app]
+# from flask import Flask
+
+
+# # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# # called `app` in `main.py`.
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     """Return a friendly HTTP greeting."""
+#     return 'Hello World!'
+
+
+from flask import Flask, escape, url_for
+from fastai.text import *
+
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+@app.route('/')
+def show_version():
+    model = simple_cnn((3,16,16,2))
+    return str(model)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
